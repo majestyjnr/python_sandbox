96feb63c8004794e0541e5a0f41db00bcdb4b7aa
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 09:51:51 -0700

    Commit Updates

--- variables.py
+++ variables.py
@@ -1,3 +1,11 @@
+# A Variable is anything that represents a value
+
+'''
+RULES
+Variabes are case sensitive (isRead and ISREAD are different values altogether)
+Variables can have a number but must not start with a number eg: name1 is valid, but 1name is invalid
+'''
+
 name = 'Developer Majesty'  # String
 x = 2                       # Int
 y = 2.5                     # Float
@@ -6,5 +14,9 @@ isRead = True               # Boolean
 # Multiple Assignments
 newName, a, b, isSeen = ('Champion Majesty', 100, 3.5, False)
 
-print(name)
+# Casting
+z = str(2)
+
+print(type(z))              # prints the type of the variable z
+print(name)                 # prints the variable name
 print(newName, isSeen)
