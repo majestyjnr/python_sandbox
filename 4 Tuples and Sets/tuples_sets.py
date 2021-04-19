dff54a4214ff99f1c33b2bd36a9513364811d28e
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 11:17:54 -0700

    Commit Updates

--- 4 Tuples and Sets/tuples_sets.py
+++ 4 Tuples and Sets/tuples_sets.py
@@ -6,8 +6,13 @@ countries = ('Ghana', 'USA', 'India', 'France', 'Nigeria', 'Belgium', 'Norepubli
 print(countries[0])         # Get a value
 print(countries)            # Prints the tuple countries
 print(type(countries))      # Prints the type of countries
-
+print(len(countries))       # Prints the length of the tuple countries
 
 
 
 # A set is a collection that is unordered or unindexed. No duplicate is allowed
+
+# Creating a SET
+countriesSet = {'Ghana', 'USA', 'India', 'France', 'Nigeria', 'Belgium', 'Norepublic', 'China'}
+
+print('USA' in countries)   # Check if in set. Display True if found, false in not found
