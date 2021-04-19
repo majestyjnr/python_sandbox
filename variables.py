d3849364c1e155fe1d70f6812173456776426df8
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 09:34:59 -0700

    Commit Updates

--- variables.py
+++ variables.py
@@ -1,5 +1,10 @@
 name = 'Developer Majesty'  # String
 x = 2                       # Int
 y = 2.5                     # Float
+isRead = True               # Boolean
+
+# Multiple Assignments
+newName, a, b, isSeen = ('Champion Majesty', 100, 3.5, False)
 
 print(name)
+print(newName, isSeen)
