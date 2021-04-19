664aecbd3546e1e896e9e1133e379007b17a8d49
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 11:07:43 -0700

    Commit Updates

--- 4 Tuples and Sets/tuples_sets.py
+++ 4 Tuples and Sets/tuples_sets.py
@@ -3,5 +3,6 @@
 # Creating a tuple
 countries = ('Ghana', 'USA', 'India', 'France', 'Nigeria', 'Belgium', 'Norepublic', 'China')
 
+print(countries[0])         # Get a value
 print(countries)            # Prints the tuple countries
 print(type(countries))      # Prints the type of countries
