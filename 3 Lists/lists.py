9e5d7cf900e7e6a0dc4ae5dcc9408f3980284e6a
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 10:45:35 -0700

    Commit Updates

---
+++ 3 Lists/lists.py
@@ -0,0 +1,6 @@
+# A list is a versatlie data type used to store a collection of simalr data items.
+
+# Creating a List
+numbers = [1, 2, 3, 4, 5, 6]
+
+print(numbers)              # printing the numbers List
