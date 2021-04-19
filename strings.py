69240dfe3d4508eb6d49a7798540c0068b0b067d
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 10:01:03 -0700

    Commit Updates

--- strings.py
+++ strings.py
@@ -1,8 +1,13 @@
 # Strings are traditionaly a sequence of characters
 
+# Below are some string formatting and method
+
 myName = 'Developer Majesty'
 workPlace = 'Maverick Edifice Limited'
 
 
 # Concatenate
 print('Hello there, My name is ' + myName + '. I am a Software Engineer at ' + workPlace + '.' )
+
+
+
