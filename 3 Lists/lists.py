3cedfccc098499a44ac17d5770512c104bd5283b
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 10:47:44 -0700

    Commit Updates

--- 3 Lists/lists.py
+++ 3 Lists/lists.py
@@ -2,5 +2,7 @@
 
 # Creating a List
 numbers = [1, 2, 3, 4, 5, 6]
+countries = ['Ghana', 'USA', 'India', 'France', 'Nigeria', 'Belgium', 'China']
 
 print(numbers)              # printing the numbers List
+print(countries(0))         # printing out the first country in the list
