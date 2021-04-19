1729fbf4fa17701131ccba7108da7b4c48c71f65
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 10:20:15 -0700

    Commit Updates

--- strings.py
+++ strings.py
@@ -4,11 +4,28 @@
 
 myName = 'Developer Majesty'
 workPlace = 'Maverick Edifice Limited'
-
+name= 'king'
 
 # Concatenate
 print('Hello there, My name is ' + myName + '. I am a Software Engineer at ' + workPlace + '.' )
 
 # Arguments by position
 print('Hello there, My name is {myName} I am a Software Engineer at {workPlace}.'.format(myName=myName, workPlace=workPlace))
+
+# F-String (This is only available in python 3.6)
+print(f'Hello there, My name is {myName} I am a Software Engineer at {workPlace}.')
+
+
+# Python String Methods
+print(name.capitalize())                                    
+
+print(name.upper())                                         
+
+print(name.lower())                                            
+
+print(name.isalpha())                                       
+
+print(name.isdigit())                                       
+
+print(myName.replace('Developer', 'Chief Programmer'))      
 
