ec17e84d2c48b69762b4d5b122c9501bdfc93dfa
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-19 10:53:11 -0700

    Commit Updates

--- 3 Lists/lists.py
+++ 3 Lists/lists.py
@@ -2,10 +2,17 @@
 
 # Creating a List
 numbers = [1, 2, 3, 4, 5, 6]
-countries = ['Ghana', 'USA', 'India', 'France', 'Nigeria', 'Belgium', 'China']
+countries = ['Ghana', 'USA', 'India', 'France', 'Nigeria', 'Belgium', 'Norepublic', 'China']
 
 # Getting the length of a list
 print(len(countries))
 
-print(numbers)                                  # printing the numbers List
+# Append to a list
+print(countries.append('Canada'))
+
+# Remove from a list
+print(countries.remove('Norepublic'))
+
+print(numbers)         
+print(countries)                         # printing the numbers List
 print(countries[0] + ' is my country')          # printing out the first country in the list
