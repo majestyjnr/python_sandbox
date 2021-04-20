aa903b000c24de51b6461b012cb7805030258aff
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-20 12:20:42 -0700

    Commit Updates

--- 7 Conditionals/conditionals.py
+++ 7 Conditionals/conditionals.py
@@ -1,1 +1,18 @@
-# 
+'''
+If/Else conditions are used to determine whether something is true or false
+Comparison Operators are normally used. They are (==, !=, >, <, >=, <= )
+'''
+
+a = 10
+b = 20
+
+# If Statement
+if(b > a):
+    print(f'{b} is greater than {a}')
+
+
+# If/Else Statement
+if(a == b):
+    print(f'{a} is equal to {b}')
+else:
+    print(f'{a} is not equal to {b}')
