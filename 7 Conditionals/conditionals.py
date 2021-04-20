d8b6ec1c8eb45577f3f9ebc0ec6378459d329737
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-20 12:55:52 -0700

    Commit Updates

--- 7 Conditionals/conditionals.py
+++ 7 Conditionals/conditionals.py
@@ -1,6 +1,8 @@
 '''
 If/Else conditions are used to determine whether something is true or false
-Comparison Operators are normally used. They are (==, !=, >, <, >=, <= )
+Comparison Operators can be used. They are (==, !=, >, <, >=, <= )
+Logical Operators can be used. They are (and, or, not)
+Membership Operators can be used. They are (not, not in)
 '''
 
 a = 10
@@ -28,3 +30,18 @@ elif(b > a):
 if(b > a):
     if(b <= 50):
        print(f'{b} is greater than {a} and less than or equal to 50')
+
+# Using the Logical Operators
+
+# Using the 'and' operator, both conditions have to be truetement to run for the conditional sta
+if(b > a and b <= 50):
+    print(f'{b} is greater than {a} and less than or equal to 50')
+
+# Using the 'or' operator, just one of the conditions ahve to run for this to be true
+if(b > a or b <= 50):
+    print(f'{b} is greater than {a} or less than or equal to 50')
+
+# not or !
+if not(a == b):
+    print(f'{a} is not equal to {b}')
+
