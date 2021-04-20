62bf4315449d0a6fcbb1f2e8f4521bd60f7bb5fc
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-20 11:25:09 -0700

    Commit Updates

---
+++ 5 Dictionaries/dictionaries.py
@@ -0,0 +1,8 @@
+# A dictionary is a collection which is unordered, changeable and indexed. No duplicate is allowed
+
+# Creating a dictionary
+staff = {
+    'name': 'Developer Majesty',
+    'proffession': 'Software Engineer',
+    'company': 'Maverick Edifice Limited',
+}
--- README.md
+++ README.md
@@ -18,3 +18,5 @@
 
 
 
+
+*To Be Updated*
