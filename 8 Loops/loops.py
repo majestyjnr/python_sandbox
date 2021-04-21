9d945d663741b5d15ca955a6cc3e1ad474ca64ed
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-21 11:41:29 -0700

    Commit Updates

---
+++ 8 Loops/loops.py
@@ -0,0 +1,9 @@
+# A loop iterates over a sequence
+
+# While loops executes a set of statements as long as the condition is true
+
+names = ['Solomon', 'Prince', 'Henry', 'Daniel', 'Samuel', 'Charles']
+
+# Simple For Loop
+for person in names:
+    print(f'Current Person is:  {person}')
