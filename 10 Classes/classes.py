9f31e3ce290ffb5bf04b91da79be1e42abce11ea
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-21 15:07:29 -0700

    Commit Updates

--- 10 Classes/classes.py
+++ 10 Classes/classes.py
@@ -8,12 +8,28 @@ class User:
         self.email = email
         self.phone = phone
 
-    # Creating a method fro the class
+    # Creating a method for the User class
     def displayInfo(self):
         return f'{self.name} is a Software Engineer at Maverick Edifice. His phone number is {self.phone}.'
+
+
+
+
+# Extending a class
+class Admin(User):
+    # Constructor (is basically a function that runs when you instantiate an object from a class )
+    def __init__(self, name, email, phone, salary):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.salary = 4500
+
+    
+
+
 
 
 # Init or Initialize User object
 majesty = User('Developer Majesty', 'developermajesty@gmail.com', '0544174142')
 
-print(majesty.displayInfo()) 
+print(majesty.displayInfo())
