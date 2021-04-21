3dc7a8a98e8a71adbd0bed216ecda10ecd536c8c
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-21 14:47:44 -0700

    Commit Updates

--- 10 Classes/classes.py
+++ 10 Classes/classes.py
@@ -2,3 +2,13 @@
 
 # Creating a class
 class User:
+    # Constructor (is basically a function that runs when you instantiate an object from a class )
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+# Init or Initialize User object
+majesty = User('Developer Majesty', 'developermajesty@gmail.com', '0544174142')
+
+print(majesty.phone)
