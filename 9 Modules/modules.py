5541d8750a67972b75f93e08a325e5d1145c45ee
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-21 12:01:44 -0700

    Commit Updates

---
+++ 9 Modules/modules.py
@@ -0,0 +1,5 @@
+# A module is a file containing a set of functions to include in your application
+# There are core python modules. Thus modules that you can install using the pip package manager
+
+# Importing a Core Python Module
+import 
