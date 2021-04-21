cf66498d12cc1494f6c84d5bca67ce816cc252db
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-21 11:46:33 -0700

    Commit Updates

--- 8 Loops/loops.py
+++ 8 Loops/loops.py
@@ -2,8 +2,20 @@
 
 # While loops executes a set of statements as long as the condition is true
 
-names = ['Solomon', 'Prince', 'Henry', 'Daniel', 'Samuel', 'Charles']
+names = ['Solomon', 'Prince', 'Henry', 'Daniel', 'Samuel', 'Charles', 'Dog', 'Tiger', 'Leopard']
 
 # Simple For Loop
+# for person in names:
+#     print(f'Current Person is:  {person}')
+
+# Break
 for person in names:
+    if (person == 'Dog'):
+        break
+    print(f'Current Person is:  {person}')
+
+# Continue
+for person in names:
+    if (person == 'Dog'):
+        continue
     print(f'Current Person is:  {person}')
