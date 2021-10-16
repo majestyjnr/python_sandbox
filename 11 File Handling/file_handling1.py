6979a9215e9e02781621eaa2c8c23fd3cec670bb
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-10-16 01:58:39 -0700

    Commit New Updates

--- 11 File Handling/file_handling1.py
+++ 11 File Handling/file_handling1.py
@@ -1,5 +1,14 @@
+# Question 1
+from datetime import datetime
+
 repository = open('date_file.txt')
 
+dictionary = {}
 for date in repository:
+    stripped_date = date.strip()
+    x = datetime.strptime(stripped_date, '%d-%b-%y')
+    (key) = stripped_date
+    (val) = x.strftime('%m-%d-%Y')
+    dictionary[key] = val
 
-    print(date)
+print(dictionary)
